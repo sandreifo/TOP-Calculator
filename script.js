8c222a6ed60a3a72693f44e9089c592f0c1bf2b0
sandreifo <s.andrei.fuentes@gmail.com>
2023-05-29 10:36:05 -0500

    Adding keyboard support

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let numbers = document.querySelectorAll(".number");
 let operations = document.querySelectorAll(".operand");
 let result = document.querySelector(".equal");
 let clear = document.querySelector(".ac");
+let dot = document.querySelector(".dot");
+let del = document.querySelector(".del");
+let percentage = document.querySelector(".percentage");
 
 let operationText = document.querySelector(".operation");
 operationText.textContent = "";
@@ -14,19 +17,60 @@ numbers.forEach((number) => {
     })
 })
 
+dot.addEventListener("click", () => {
+    let opString = operationText.textContent;
+    array = opString.split(" ");
+    let a = array[0];
+    let op = array[1];
+    let b = array[2];
+
+    if (!b) {
+        if (a.indexOf(".") === -1) {
+            operationText.textContent = operationText.textContent + dot.textContent;
+        }
+    } else {
+        if (b.indexOf(".") === -1) {
+            operationText.textContent = operationText.textContent + dot.textContent;
+        }
+    }
+})
+
+percentage.addEventListener("click", () => {
+    let opString = operationText.textContent;
+    array = opString.split(" ");
+    let a = array[0];
+    let op = array[1];
+    let b = array[2];
+    if (!op) {
+        a = a/100;
+        operationText.textContent = a;
+    } else if (op == "+" || op == "-"){
+        let c = (b*a)/100;
+        operationText.textContent = a + " " + op + " " + c + " (" + b + "%)";
+    } else {
+        b = b/100;
+        operationText.textContent = a + " " + op + " " + b;
+    }
+})
+
 operations.forEach((operation) => {
     operation.addEventListener("click", () => {
-        operationText.textContent = operationText.textContent + " " + operation.textContent + " ";
+        if (operationText.textContent.indexOf("+") != -1 ||
+            operationText.textContent.indexOf("-") != -1 ||
+            operationText.textContent.indexOf("x") != -1 ||
+            operationText.textContent.indexOf("/") != -1
+            ) 
+        {
+            resultText.textContent = Math.floor(operationx()*1000)/1000;
+            operationText.textContent = resultText.textContent + " " + operation.textContent + " ";
+        } else {
+            operationText.textContent = operationText.textContent + " " + operation.textContent + " ";
+        }
     })
 })
 
 result.addEventListener("click", () => {
-    let opString = operationText.textContent;
-    array = opString.split(" ");
-    let num1 = array[0];
-    let op = array[1];
-    let num2 = array[2];
-    resultText.textContent = operation (num1, num2, op);
+    resultText.textContent = Math.floor(operationx()*1000)/1000;
 })
 
 clear.addEventListener("click", () => {
@@ -34,8 +78,27 @@ clear.addEventListener("click", () => {
     resultText.textContent = "";
 })
 
-function operation (a, b, op) {
-    console.log(op);
+del.addEventListener("click", () => {
+    operationText.textContent = operationText.textContent.slice(0,operationText.textContent.length - 1);
+})
+
+function operationx () {
+    let opString = operationText.textContent;
+    array = opString.split(" ");
+    let a = array[0];
+    let op = array[1];
+    let b = array[2];
+
+    if ((op == "x" || op == "/") && b == "") {
+        b = 1
+    }
+
+    if(op == "/" && b == 0) {
+        alert ("please don't")
+        operationText.textContent = ""
+        return 0
+    }
+    
     if (op == "x") {
         return a*b;
     } else if (op == "/") {
@@ -46,3 +109,25 @@ function operation (a, b, op) {
         return a-b;
     }
 }
+
+window.addEventListener("keydown", (e) => {
+    if (!isNaN(e.key) && e.key != " ") {
+        document.getElementById(`num${e.key}`).click();
+    } else if (e.key === "+") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "-") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "*") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "/") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "Enter") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "Backspace") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === "%") {
+        document.getElementById(`key${e.key}`).click();
+    } else if (e.key === " ") {
+        document.getElementById(`key${e.key}`).click();
+    }
+})
